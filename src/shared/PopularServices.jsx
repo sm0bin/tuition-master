3660ec1af7b355ecd9bb9e2d9d424bf482472241
GhostMobin <s1810677110@ru.ac.bd>
2023-11-07 14:56:56 +0600

    popular services section

--- src/shared/PopularServices.jsx
+++ src/shared/PopularServices.jsx
@@ -12,27 +12,26 @@ const PopularServices = () => {
     return (
         <div>
 
-
-            <div className="max-w-7xl mx-4 md:mx-8 lg:mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <h2 className="font-bold text-center text-3xl mb-12">Popular Services</h2>
+            <div className="max-w-7xl mx-4 md:mx-8 lg:mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
                 {
                     services?.slice(0, 4).map(service => (
 
 
-                        <div key={service._id} className="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#">
-                                <img className="rounded-t-lg w-full h-52 object-cover" src={service.serviceImage} alt={`${service?.serviceName} image`} />
-                            </a>
-                            <div className="p-5 flex flex-col flex-grow" >
-                                <div className="flex-grow">
 
-                                    <a href="#">
-                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{service?.serviceName}</h5>
-                                    </a>
+
+                        <div key={service._id} className="flex overflow-hidden flex-col items-start bg-white border border-gray-200 rounded-lg shadow  w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <img className="object-cover w-full h-[400px] rounded-t-lg  md:rounded-none" src={service.serviceImage} alt={`${service?.serviceName} image`} />
+                            <div className="flex flex-col  justify-between p-6 leading-normal space-y-3 grow">
+                                <div className="grow">
+
+                                    <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{service?.serviceName}</h5>
                                     <div className="flex items-center gap-3">
-                                        <img className="w-10 h-10 rounded-full object-cover" src={service?.serviceProvider?.image} alt={`${service?.serviceProvider?.name} image`} />
+                                        <img className="w-12 h-12 rounded-full object-cover" src={service?.serviceProvider?.image} alt={`${service?.serviceProvider?.name} image`} />
                                         <h4 className="font-bold text-lg">{service?.serviceProvider?.name}</h4>
                                     </div>
-                                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Price: ${service?.servicePrice}</h5>
+                                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Price: ${service?.servicePrice}</h5>
+                                    <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Service Area: {service?.serviceArea}</h5>
                                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{service?.serviceDescription}</p>
                                 </div>
                                 <Link href="#" className="w-max inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -43,6 +42,9 @@ const PopularServices = () => {
                                 </Link>
                             </div>
                         </div>
+
+
+
 
                     ))
                 }
